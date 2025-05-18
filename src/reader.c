a8c0ece79c6d9b504927bf0a1d76d87aea1c480e
Leon <132808925+Leon8326@users.noreply.github.com>
2025-05-18 20:14:02 +0200

    Update reader.c

--- src/reader.c
+++ src/reader.c
@@ -8,7 +8,6 @@ int main(int argc, char *argv[]) {
     const char *prompt = ">> ";
     const char *history_file = NULL;
 
-    // Argument parsing
     for (int i = 1; i < argc; ++i) {
         if ((strcmp(argv[i], "-e") == 0 || strcmp(argv[i], "--history") == 0) && (i + 1 < argc)) {
             history_file = argv[++i];
@@ -17,23 +16,20 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Load history if specified
     if (history_file) {
         read_history(history_file);
     }
 
-    // Read user input
     char *input = readline(prompt);
 
     if (input) {
-        // Save non-empty input to history
         if (*input && history_file) {
             add_history(input);
             write_history(history_file);
         }
 
-        // Print input to stdout (capturable by shell)
-        printf("%s\n", input);
+        // Only print the input!
+        puts(input);
         free(input);
     }
 
