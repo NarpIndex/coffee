242b1d58b9f764769a8eb1a629bee8316baa7a8c
Leon <132808925+Leon8326@users.noreply.github.com>
2025-05-18 20:24:55 +0200

    Update install.sh

--- project/install.sh
+++ project/install.sh
@@ -2,4 +2,5 @@
 
 sudo ln -sf /usr/bitey/software/coffee/bin/csh /usr/bin/csh
 sudo ln -sf /usr/bitey/software/coffee/bin/coffee /usr/bin/coffee
+sudo ln -sf /usr/bitey/software/coffee/bin/reader /usr/bin/reader
 grep -qxF "/usr/bin/csh" /etc/shells || echo "/usr/bin/csh" | sudo tee -a /etc/shells
